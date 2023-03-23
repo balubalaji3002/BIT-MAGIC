5c62050507c902026867a55af17a7904bcd5219a
k.Balaji <66003194+balubalaji3002@users.noreply.github.com>
2023-03-23 07:37:35 +0530

    Create Swap-Two-Numbers-Without-using-Third-Variable.cpp

---
+++ Swap-Two-Numbers-Without-using-Third-Variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5,b=7;
+    cout<<"a--"<<a<<"  b--"<<b<<endl;
+    a=a^b;
+    b=b^a;
+    a=a^b;
+    cout<<"a--"<<a<<"  b--"<<b;
+  return 0;
+}
