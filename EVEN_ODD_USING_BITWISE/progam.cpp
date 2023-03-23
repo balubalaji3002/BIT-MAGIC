0be04dcf0376350b2e533e4f9166e5a2b97a4eff
k.Balaji <66003194+balubalaji3002@users.noreply.github.com>
2023-03-23 07:51:39 +0530

    Create progam.cpp

---
+++ EVEN_ODD_USING_BITWISE/progam.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=84;
+    if(n&1)
+    {
+        cout<<"odd";
+    }
+    else{
+        cout<<"even";
+    }
+    return 0;
+    
+}
